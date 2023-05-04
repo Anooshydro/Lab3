40ca84f291cdde9feb879ce29b1aa1764fb88037
Jaedon <jaedonseng69.22@ichat.sp.edu.sg>
2023-05-04 15:31:36 +0800

    first commit

--- lab2.py
+++ lab2.py
@@ -6,14 +6,16 @@ def calculate_bmi (height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     elif bmi > 25.0:
         print("Over Weight")
+        return 1
     elif 18.5 <= bmi <= 25.0:
         print("Normal Weight")
+        return 0
     else:
         print("Error")
 
 
-
-
-calculate_bmi(weight=65, height=1.75)
+value=calculate_bmi(weight=65, height=1.75)
+print(value)
